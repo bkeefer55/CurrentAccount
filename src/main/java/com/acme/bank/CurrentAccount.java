193fb77859f8849198473c3c90877b4b7ea5e712
Billy <bkeefer55@gmail.com>
2020-06-03 18:28:39 +0100

    US0002 - updating simple output statement

---
+++ src/main/java/com/acme/bank/CurrentAccount.java
@@ -0,0 +1,13 @@
+package com.acme.bank;
+
+/**
+ * Hello world!
+ *
+ */
+public class CurrentAccount 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/acme/bank/CurrentAccountTest.java
@@ -0,0 +1,20 @@
+package com.acme.bank;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CurrentAccountTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
